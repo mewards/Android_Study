15b0284caec94ca781c8542082217170fd4d33ef
wooil <wooil.k.89@gmail.com>
2014-03-23 11:35:51 +0900

    hw6

--- hw/wooil/hw5/src/com/mewards/hw5/MainActivity.java
+++ hw/wooil/hw5/src/com/mewards/hw5/MainActivity.java
@@ -1,10 +1,12 @@
 package com.mewards.hw5;
 
 import android.os.Bundle;
+import android.os.Debug;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -16,6 +18,7 @@ public class MainActivity extends Activity implements OnClickListener {
 	int no1 = 0, no2 = 0, no3 = 0, no4 = 0;
 	TextView txt1, txt2, txt3, txt4;
 	IncThread thr1, thr2, thr3, thr4;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +29,7 @@ public class MainActivity extends Activity implements OnClickListener {
 		txt2 = (TextView) findViewById(R.id.no2);
 		txt3 = (TextView) findViewById(R.id.no3);
 		txt4 = (TextView) findViewById(R.id.no4);
-		
+
 		txt1.setText(no1 + "");
 		txt2.setText(no2 + "");
 		txt3.setText(no3 + "");
@@ -35,26 +38,26 @@ public class MainActivity extends Activity implements OnClickListener {
 		Button btn = (Button) findViewById(R.id.button1);
 		btn.setOnClickListener(this);
 
-		thr1 = new IncThread(no1, txt1);
-		thr2 = new IncThread(no2, txt2);
-		thr3 = new IncThread(no3, txt3);
-		thr4 = new IncThread(no4, txt4);
+		thr1 = new IncThread(no1, txt1, 1);
+		thr2 = new IncThread(no2, txt2, 2);
+		thr3 = new IncThread(no3, txt3, 3);
+		thr4 = new IncThread(no4, txt4, 4);
 
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
 	class IncThread extends Thread {
-
 		int no;
 		TextView txt;
+		int val;
+		String a;
 
-		public IncThread(int no, TextView txt) {
+		public IncThread(int no, TextView txt, int val) {
 			this.no = no;
 			this.txt = txt;
 		}
@@ -62,23 +65,23 @@ public class MainActivity extends Activity implements OnClickListener {
 		public void run() {
 			while (true) {
 				no++;
-				handler.sendEmptyMessage(0);
 				try {
 					Thread.sleep(1000);
 				} catch (InterruptedException e) {
-					;
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						a = txt.getText().toString();
+						Log.e("testtest : ",a);
+						txt.setText(no+"");
+						
+					}
+				});
 			}
 		}
-
-		Handler handler = new Handler() {
-			public void handleMsg(Message msg) {
-				if (msg.what == 0) {
-					txt.setText(no + "");
-				}
-			}
-		};
-
 	}
 
 	@Override
@@ -86,10 +89,10 @@ public class MainActivity extends Activity implements OnClickListener {
 		switch (v.getId()) {
 		case R.id.button1:
 
-			thr1.run();
-			// thr2.run();
-			// thr3.run();
-			// thr4.run();
+			thr1.start();
+			thr2.start();
+			thr3.start();
+			thr4.start();
 			break;
 		}
 	}
